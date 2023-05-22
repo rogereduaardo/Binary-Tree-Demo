c335c4927b70bdce5ffa262d626e8dd5979ba9f5
Rogereduaardo <remmtool98@gmail.com>
2023-05-22 14:37:42 +0000

    data structure defined

--- binary-tree.c
+++ binary-tree.c
@@ -1,10 +1,25 @@
 // binary-tree.c
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "binary-tree.h"
 
+typedef struct node_
+{
+  int data;
+  struct node_ *left;
+  struct node_ *right;
+}node_t;
+
+typedef struct root_
+{
+node_t *node;
+}root_t;
+
+static root_t *root;
+
 binary_tree_handle binary_tree_init()
 {
-  return NULL;
+  return calloc(1, sizeof(root_t));
 }
